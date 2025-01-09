5efbb3078c86c255b0dff6afe04a6ef92328ec27
Thisara Mendis <161597652+thisara18@users.noreply.github.com>
2025-01-09 21:34:07 +0530

    Add files via upload

---
+++ footer/footer.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
+</head>
+
+<body>
+    <footer class="Footer">
+
+        <div class="footer-Top">
+            <div class="footer-left">
+
+                <img src="logo.png" alt="logo image">
+                <p>Discovering the hidden gem of great
+                    food in yakkala has been an
+                    absulate delight from severy local flavors to international cusine</p>
+                <div class="Social-Links ">
+                    <a href="https://www.facebook.com/melissas.yakkala?mibextid=ZbWKwL"><i
+                            class="ri-facebook-circle-fill"></i></a>
+                    <a href="https://www.instagram.com/melissas_yakkala?igsh=MWlocXhpNmszY2hwYg=="><i
+                            class="ri-instagram-fill"></i></a>
+                    <a href="https://youtube.com/@MelissasFoodCourt?si=4XP5nuQomf3DivRX"><i
+                            class="ri-youtube-fill"></i></a>
+                    <a href="https://g.co/kgs/wPs8nTd"><i class="ri-google-fill"></i></a>
+                </div>
+            </div>
+
+
+            <div class="footer-center">
+
+                <div class="Details">
+                    <div>
+                        <p>
+                        <h4><u>CONTACT</u></h4>
+                        </p>
+                    </div>
+                    <p><i class="ri-phone-fill px-2"></i>+94 70 426 0260</p><br>
+                    <p><i class="ri-google-fill px-2"></i>melissas@gmail.com</p>
+                    <p><i class="ri-map-pin-fill px-2"></i>No 151 Kandy Rd ,Yakkala , Sri Lanka</p>
+                    <div>
+                        <p>
+                        <h4><u>OPENING HOURSE</u></h4>
+                        </p>
+                    </div>
+                    <p><i class="ri-store-3-fill px-2"></i>Monday - Friday 7.00 am -11.00 pm</p>
+                    <p><i class="ri-store-3-line px-2"></i>Suterday - Sunday : 7.00 am -11.00 pm</p>
+                </div>
+            </div>
+
+            <div class="footer-right">
+
+
+                <div class="footer-map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.2680033871725!2d80.03730407365418!3d7.0949026162623445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2fd58661c0823%3A0x55ba8bed07c1eda5!2sMelissa&#39;s%20Food%20Court!5e0!3m2!1sen!2slk!4v1712627007396!5m2!1sen!2slk"
+                        width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="footer-divider">
+            <div class="divider-left"><p>Copy right all reserverd</p></div>
+            <div class="divider-right"><p>Design by EPMAC IT</p></div>
+
+
+        </div>
+    </footer>
+</body>
+
+</html>
